ec2edbf9e548fcb11e5397ae5ebd6c9dd031644d
Guy Chaplin <guy.chaplin@googlemail.com>
2021-03-28 11:46:55 +0100

    Adds labels, radio buttons for correct_answer and form as table. Plus create_question html missed on previous commit.

--- capstone/forms.py
+++ capstone/forms.py
@@ -2,7 +2,20 @@ from django import forms
 from .models import Question
 
 
+CHOICES = [('answer0', 'Answer A'), ('answer1', 'Answer B'),
+           ('answer2', 'Answer C'), ('answer3', 'Answer D')]
+
+
 class QuestionCreateForm(forms.ModelForm):
+    correct_answer = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect())
+
     class Meta:
         model = Question
         exclude = ['user', 'timestamp']
+        labels = {
+            "content": "Question",
+            "answer0": "Answer A",
+            "answer1": "Answer B",
+            "answer2": "Answer C",
+            "answer3": "Answer D",
+        }
