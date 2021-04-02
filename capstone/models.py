8ec72d5ee9be917fc5ce7cdac75cb3fcf80c174f
Guy Chaplin <guy.chaplin@googlemail.com>
2021-04-02 11:41:38 +0100

    adds quiz model

--- capstone/models.py
+++ capstone/models.py
@@ -20,3 +20,14 @@ class Question(models.Model):
 
     def __str__(self):
         return f"{self.user.username} at {self.timestamp}. Question: {self.content} with answers: {self.answer0}, {self.answer1}, {self.answer2}, {self.answer3}. Correct answer: {self.correct_answer}"
+
+
+class Quiz(models.Model):
+    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="quiz_master")
+    timestamp = models.DateTimeField(auto_now_add=True)
+    quiz_name = models.CharField(max_length=30, unique=True)
+    questions = models.ManyToManyField("Question")
+
+    def __str__(self):
+        first_question = self.questions.all().first()
+        return f"{self.user.username} at {self.timestamp}. Quizname: {self.quiz_name}. First question: {first_question.content}"
