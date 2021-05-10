39728ece094fda4ae121d52af04d7088e4065a77
Guy Chaplin <guy.chaplin@googlemail.com>
2021-05-10 12:28:18 +0100

    adds condition to limit Next button append to once. Adds if...else to control end of quiz when all questions completed. Issue: can't redirect to 'quiz_end' page.

--- capstone/static/capstone/play_quiz.js
+++ capstone/static/capstone/play_quiz.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
     /* Define question index. From 0 to N-1. Where N is the number of questions in the quiz.*/
     var n = 0;
+    /* Define N, the number of questions in the quiz */
+    /* TODO. Get value of N from html page. TODO. First get value of N from count() in play_quiz route */
+    var N = 10;
     document.querySelectorAll('.multi_choice').forEach(button => {
         button.onclick = () => {
             /* Button clicked stays hover colour */
@@ -35,66 +38,81 @@ document.addEventListener('DOMContentLoaded', () => {
             const next_btn = document.createElement("button");
             next_btn.innerHTML = "Next";
 
-            /* Append button to #contestant paragraph */
-            document.querySelector('#contestant').append(next_btn);
-
+            /* Append button to #contestant paragraph. Append is done once for question
+            index n less than or equal to zero */
+            if (n <= 0) {
+                document.querySelector('#contestant').append(next_btn);
+            }
+            /* console.log("n = " + n); */
             /* next_btn onclick function */
             next_btn.addEventListener('click', function()   {
                 /* increase question index by one */
                 n += 1;
+                /* Test if at the end of the quiz. If beyond last question open quiz_end page */
+                if (n <= N-1)   {
 
-                /* Get contestant_id from document */
-                const contestant_id = document.querySelector('#contestant').dataset.id;
+                    /* Get contestant_id from document */
+                    const contestant_id = document.querySelector('#contestant').dataset.id;
 
-                /* Ensure id is integer */
-                const id = parseInt(contestant_id);
-                /* const id = 4; */
+                    /* Ensure id is integer */
+                    const id = parseInt(contestant_id);
+                    /* const id = 4; */
 
-                console.log(id);
+                    console.log(id);
 
-                /* PUT request to supply question index n */
-                /* Note: the marks around the path are backticks not single quotation marks */
-                fetch(`/play_quizAPI/${id}`, {
-                    method:'PUT',
-                    body: JSON.stringify({
-                        question_index: n
-                    })
-                })
-                .then(response => response.json())
-                .then(result => {
-                    console.log(result);
-
-                    /* GET request to retrieve question and answers with index n */
-                    fetch(`/play_quizAPI/${id}`,    {
-                        method: 'GET'
+                    /* PUT request to supply question index n */
+                    /* Note: the marks around the path are backticks not single quotation marks */
+                    fetch(`/play_quizAPI/${id}`, {
+                        method:'PUT',
+                        body: JSON.stringify({
+                            question_index: n
+                        })
                     })
                     .then(response => response.json())
-                    .then(contestant => {
-                        console.log(contestant.question);
-                        console.log(contestant.multiple_choice0);
+                    .then(result => {
+                        console.log(result);
 
-                    /* Pick up id for question and replace innerHTML */
-                        document.querySelector('#question').innerHTML = contestant.question;
-                    /* Pick up span id for multiple choice answer and replace innerHTML */
-                        document.querySelector('#answer0').innerHTML = contestant.multiple_choice0;
-                        document.querySelector('#answer1').innerHTML = contestant.multiple_choice1;
-                        document.querySelector('#answer2').innerHTML = contestant.multiple_choice2;
-                        document.querySelector('#answer3').innerHTML = contestant.multiple_choice3;
-                    /* Add data for correct answer */
-                        document.querySelector('#correct_answer').dataset.answer = contestant.correct_answer;
+                        /* GET request to retrieve question and answers with index n */
+                        fetch(`/play_quizAPI/${id}`,    {
+                            method: 'GET'
+                        })
+                        .then(response => response.json())
+                        .then(contestant => {
+                            console.log(contestant.question);
+                            console.log(contestant.multiple_choice0);
+
+                        /* Pick up id for question and replace innerHTML */
+                            document.querySelector('#question').innerHTML = contestant.question;
+                        /* Pick up span id for multiple choice answer and replace innerHTML */
+                            document.querySelector('#answer0').innerHTML = contestant.multiple_choice0;
+                            document.querySelector('#answer1').innerHTML = contestant.multiple_choice1;
+                            document.querySelector('#answer2').innerHTML = contestant.multiple_choice2;
+                            document.querySelector('#answer3').innerHTML = contestant.multiple_choice3;
+                            /* Add data for correct answer */
+                            document.querySelector('#correct_answer').dataset.answer = contestant.correct_answer;
+                        });
+                    /* All multi_choice buttons are reenabled after new question setup.*/
+                    /* All multi_choice buttons background colour set to red */
+                    document.querySelectorAll('.multi_choice').forEach(button => {
+                        button.disabled = false;
+                        button.style.backgroundColor="#D73338";
+                        /* button.style:hover.backgroundColor="#4C8100"; */
+                        })
+
                     });
-                /* All multi_choice buttons are reenabled after new question setup.*/
-                /* All multi_choice buttons background colour set to red */
-                document.querySelectorAll('.multi_choice').forEach(button => {
-                    button.disabled = false;
-                    button.style.backgroundColor="#D73338";
-                    /* button.style:hover.backgroundColor="#4C8100"; */
-                    })
 
-                });
+                }
+                /* If beyond last question open quiz_end page */
+                else {
+                    /* This call to open new page is not working. URL helper doesn't work in JavaScript??*/
+                    document.location.href = "{% url 'index' %}";
+                    /* This alternative works...*/
+                    /* document.location.href = "http://localhost:8000/"; */
+                }
+
             })
 
-            /* test: console.log(button.dataset.answer); */
+                /* test: console.log(button.dataset.answer); */
         };
     });
 });
