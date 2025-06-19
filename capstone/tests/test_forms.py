28b71e607412d4b4192e5ed1c31cc3f798090d6a
Guy Chaplin <guy.chaplin@googlemail.com>
2025-06-19 15:35:16 +0100

    Adds tests for QuizCreate, ContestSelect and ResultsSelect forms

--- capstone/tests/test_forms.py
+++ capstone/tests/test_forms.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from capstone.forms import QuestionCreateForm
+from capstone.forms import QuestionCreateForm, QuizCreateForm, ContestSelectForm, ResultsSelectForm
 
 # Create your tests here.
 
@@ -33,3 +33,25 @@ class TestQuestionCreateForm(TestCase):
     def test_answer3_field_label(self):
         form = QuestionCreateForm()
         self.assertTrue(form.fields['answer3'].label is None or form.fields['answer3'].label == 'Answer D')
+
+    # More tests.
+    #       Test_correct_answer_form_field_shows_radio_select_widget
+
+class TestQuizCreateForm(TestCase):
+    def test_empty_form(self):
+        form = QuizCreateForm()
+        self.assertIn("quiz_name", form.fields)
+        self.assertIn("questions", form.fields)
+
+    # More tests.
+    #       Test_questions_form_field_shows_check_box_select_multiple_widget
+
+class TestContestSelectForm(TestCase):
+    def test_empty_form(self):
+        form = ContestSelectForm()
+        self.assertIn("quiz", form.fields)
+
+class TestResultsSelectForm(TestCase):
+    def test_empty_form(self):
+        form = ResultsSelectForm()
+        self.assertIn("quiz", form.fields)
