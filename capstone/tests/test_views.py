7c9fcc5f55f7db006fc2c712cfa1d988a5722680
Guy Chaplin <guy.chaplin@googlemail.com>
2025-07-05 10:55:15 +0100

    Adds tests for QuizCreateView

--- capstone/tests/test_views.py
+++ capstone/tests/test_views.py
@@ -126,3 +126,42 @@ class CreateQuestionViewDatabaseTest(TestCase):
 
         # Ensure no question created in the database
         self.assertFalse(Question.objects.exists())
+
+class CreateQuizViewTest(TestCase):
+    def setUp(self):
+        # Create a user
+        test_user1 = User.objects.create_user(username = 'testuser1', password='testpassword')
+        test_user1.save()
+
+        # Create question
+        test_question = Question.objects.create(user=test_user1, content='Is this a test question?', answer0='Yes', answer1='No', answer2='No', answer3='No', correct_answer='answer0')
+
+        test_question.save()
+
+        # Create quiz
+        test_quiz = Quiz.objects.create(user=test_user1, quiz_name='test_quiz1')
+        test_quiz.questions.add(test_question)
+        test_quiz.save()
+
+    def test_redirect_if_not_logged_in(self):
+        # Get response
+        response = self.client.get(reverse("create_quiz"))
+        self.assertRedirects(response, "/login?next=/create_quiz/")
+
+    def test_logged_in_user_uses_correct_template(self):
+        # Login and get response
+        login = self.client.login(username = 'testuser1', password='testpassword')
+        response = self.client.get(reverse("create_quiz"))
+
+        # Check our user is logged in
+        self.assertEqual(str(response.context['user']), "testuser1")
+
+        # Check that we got a response "success"
+        self.assertEqual(response.status_code, 200) 
+
+        # Check that we used correct template
+        self.assertTemplateUsed(response, "capstone/create_quiz.html")
+
+    # Add tests for list of questions in form checkbox
+    # More tests.
+    #       Test_questions_form_field_shows_check_box_select_multiple_widget
