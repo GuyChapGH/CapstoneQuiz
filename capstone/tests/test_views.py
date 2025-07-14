406fa208a4f28d34ff1452ca37f014c89c41434c
Guy Chaplin <guy.chaplin@googlemail.com>
2025-07-14 10:27:07 +0100

    Adds redirect and correct template tests for play_quiz view

--- capstone/tests/test_views.py
+++ capstone/tests/test_views.py
@@ -190,7 +190,8 @@ class CreateQuizViewDatabaseTest(TestCase):
 
         response = self.client.post(reverse('create_quiz'), data=form_data)
 
-        # Don't know how to create test Quiz object. 
+        # NB: THIS TEST DOESNT WORK. 
+        #  Don't know how to create test Quiz object. 
         # form_data approach does not work because questions is a many-to-many field
         # many-to-many field requires instance to be created in database first
         # Don't know how to reference this object in database so can 'add' questions.
@@ -246,32 +247,20 @@ class QuizSelectViewDatabaseTest(TestCase):
         test_quiz = Quiz.objects.create(user=test_user1, quiz_name='test_quiz1')
         test_quiz.questions.add(test_question)
         test_quiz.save()
-
-        # Create contest
-        #test_contest = Contest.objects.create(user=test_user1, #quiz=test_quiz)
-        #test_contest.save()
         
         form_data = {
-            #'user': test_user1,
             'quiz': test_quiz           
         }
 
         response = self.client.post(reverse('quiz_select'), data=form_data)
 
         # Check that the contest was created and we were redirected
-        # self.assertEqual(response.status_code, 302) # Redirect after form submission
-        # Try to see what from error we have as Contest not created in database
+        self.assertEqual(response.status_code, 302) # Redirect after form submission
+        
+        # Contest should be in the database
+        self.assertTrue(Contest.objects.exists())        
 
-        #self.assertFalse(Quiz.objects.exists()) # See if quiz successfully created
-
-        # Check error messages in form response
-        #self.assertTrue("form" in response.context)
-
-        #form = response.context['form']
-
-        #self.assertFormError(form, 'quiz', 'This field is required.')
-
-        self.assertTrue(Contest.objects.exists()) # Contest should be in the database       
+        # NB: THIS TEST DOESNT WORK. form_data doesnt select test_quiz and Contest object is not created.
 
     def test_doesnt_create_contest_and_returns_to_form_when_submitting_invalid_form(self):
         """Test that form submission with invalid data does not create a contest in the database"""
@@ -311,3 +300,46 @@ class QuizSelectViewDatabaseTest(TestCase):
         self.assertFormError(form, 'quiz', 'This field is required.')
 
         self.assertFalse(Contest.objects.exists()) # No Contest should be in the database 
+
+class PlayQuizViewTest(TestCase):
+    def setUp(self):
+        # Create a user
+        test_user1 = User.objects.create_user(username = 'testuser1', password='testpassword')
+        test_user1.save()
+
+        # Create question
+        test_question = Question.objects.create(user=test_user1, content='Is this a test question?', answer0='Yes', answer1='No', answer2='No', answer3='No', correct_answer='answer0')
+
+        test_question.save()
+
+        # Create quiz
+        test_quiz = Quiz.objects.create(user=test_user1, quiz_name='test_quiz1')
+        test_quiz.questions.add(test_question)
+        test_quiz.save()
+
+        # Create contest
+        test_contest = Contest.objects.create(user=test_user1, quiz=test_quiz)
+        test_contest.save()
+
+    def test_redirect_if_not_logged_in(self):
+        # Get response
+        contest_id = Contest.objects.all().last().id
+        response = self.client.get(reverse("play_quiz", args=(contest_id,)))
+
+        self.assertRedirects(response, "/login?next=/play_quiz/1")
+
+    def test_logged_in_user_uses_correct_template(self):
+        # Login and get response
+        login = self.client.login(username = 'testuser1', password='testpassword')
+        
+        contest_id = Contest.objects.all().last().id
+        response = self.client.get(reverse("play_quiz", args=(contest_id,)))
+
+        # Check our user is logged in
+        self.assertEqual(str(response.context['user']), "testuser1")
+
+        # Check that we got a response "success"
+        self.assertEqual(response.status_code, 200) 
+
+        # Check that we used correct template
+        self.assertTemplateUsed(response, "capstone/play_quiz.html")
