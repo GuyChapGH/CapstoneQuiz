1e1934e497c8619daa4e5d0fa358d4870e88e905
Guy Chaplin <guy.chaplin@googlemail.com>
2025-08-02 11:45:50 +0100

    Adds working tests for create quiz view database test

--- capstone/tests/test_views_create_quiz.py
+++ capstone/tests/test_views_create_quiz.py
@@ -63,19 +63,94 @@ class CreateQuizViewDatabaseTest(TestCase):
         form_data = {
             'user': test_user1,
             'quiz_name': 'test_quiz1', 
-            #'questions': test_question
+            'questions': 1
         }
 
         response = self.client.post(reverse('create_quiz'), data=form_data)
 
-        # NB: THIS TEST DOESNT WORK. 
-        #  Don't know how to create test Quiz object. 
-        # form_data approach does not work because questions is a many-to-many field
-        # many-to-many field requires instance to be created in database first
-        # Don't know how to reference this object in database so can 'add' questions.
-        # ????.questions.add(test_question)
-        # ????.save()
-
         # Check that the quiz was created and we were redirected
-        self.assertEqual(response.status_code, 302) # Redirect after form submission
+        self.assertEqual(response.status_code, 302) # Redirects after successful form submission
         self.assertTrue(Quiz.objects.filter(quiz_name='test_quiz1').exists()) # Quiz should be in the database
+
+    def test_dont_create_quiz_when_submitting_invalid_form(self):
+        """Test that form submission with invalid data fails to create a quiz in the database"""
+  
+        # Create a user
+        test_user1 = User.objects.create_user(username = 'testuser1', password='testpassword')
+        test_user1.save()
+
+        # Login user
+        login = self.client.login(username = 'testuser1', password='testpassword')
+
+        # Create question
+        test_question = Question.objects.create(user=test_user1, content='Is this a test question?', answer0='Yes', answer1='No', answer2='No', answer3='No', correct_answer='answer0')
+
+        test_question.save()
+
+        form_data = {
+            'user': test_user1,
+            'quiz_name': '', # quiz_name required so should fail 
+            'questions': 1
+        }
+
+        response = self.client.post(reverse('create_quiz'), data=form_data)
+
+
+        # Check that the quiz failed to create and we stay on page with error message
+        self.assertEqual(response.status_code, 200) # Stays on page after failed form submission showing errors
+
+        # Check error messages in form response
+        self.assertTrue("form" in response.context)
+
+        form = response.context['form']
+
+        # Test for error message
+        self.assertEqual(form.errors['quiz_name'][0], 'This field is required.')
+
+        # Test no quiz in database
+        self.assertFalse(Quiz.objects.exists()) # There should be no quiz in the database
+
+    def test_dont_create_quiz_when_submitting_duplicate_quiz_name_invalid_form(self):
+        """Test that form submission with duplicate quiz_name invalid data fails to create a quiz in the database"""
+  
+        # Create a user
+        test_user1 = User.objects.create_user(username = 'testuser1', password='testpassword')
+        test_user1.save()
+
+        # Login user
+        login = self.client.login(username = 'testuser1', password='testpassword')
+
+        # Create question
+        test_question = Question.objects.create(user=test_user1, content='Is this a test question?', answer0='Yes', answer1='No', answer2='No', answer3='No', correct_answer='answer0')
+
+        test_question.save()
+
+        # Create quiz
+        test_quiz = Quiz.objects.create(user=test_user1, quiz_name='test_quiz1')
+        test_quiz.questions.add(test_question)
+        test_quiz.save()
+
+
+        # Set up data for POST to view
+        form_data = {
+            'user': test_user1,
+            'quiz_name': 'test_quiz1', # quiz_name duplicated so should fail 
+            'questions': 1
+        }
+
+        response = self.client.post(reverse('create_quiz'), data=form_data)
+
+
+        # Check that the quiz failed to create and we stay on page with error message
+        self.assertEqual(response.status_code, 200) # Stays on page after failed form submission showing errors
+
+        # Check error messages in form response
+        self.assertTrue("form" in response.context)
+
+        form = response.context['form']
+
+        # Test for error message
+        self.assertEqual(form.errors['quiz_name'][0], 'Quiz with this Quiz name already exists.')
+
+        # Test only one quiz in database
+        self.assertEqual(Quiz.objects.count(), 1) # There should only be one quiz in the database (created in set up)
